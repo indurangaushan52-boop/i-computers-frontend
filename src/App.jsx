ae75471d4aab5957aba45cd042fb522772270a70
UshanFernando <indurangaushan52@gmail.com>
2026-07-14 23:34:51 +0530

    Change Locations of Divisions👌

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import TrendingProducts from './components/trendingProducts'
+import HomePage from './pages/homePage'
+import AdminPage from './pages/adminPage'
+
 
 function App() {
   return (
-    <>
-      <div> 
+      <div className = "w-full h-screen flex justify-center items-center bg-primary text-secondary"> 
+        <Routes>
+          <Route path = "/" element = {<HomePage/>} />
+          <Route path = "/admin/*" element = {<AdminPage/>} />
 
-        <TrendingProducts/>
-        
+        </Routes>
       </div>
-    </>
   )
 }
 
---
+++ src/pages/adminPage.jsx
@@ -0,0 +1,32 @@
+import { Link, Route, Routes } from "react-router-dom";
+
+export default function AdminPage(){
+    return(
+        <div className = "w-full h-screen flex items-center bg-accent ">
+            <div className="w-[300px] h-full bg-accent text-white">
+                
+                {/*<a href="/admin/" className = "block py-2 px-4 hover:bg-gray-700">Orders</a>
+                <a href="/admin/products" className = "block py-2 px-4 hover:bg-gray-700">Products</a>
+                <a href="/admin/users" className = "block py-2 px-4 hover:bg-gray-700">Users</a>
+                <a href="/admin/reviews" className = "block py-2 px-4 hover:bg-gray-700">Reviews</a>*/}
+
+                <Link to="/admin/" className = "block py-2 px-4 hover:bg-gray-700">Orders</Link>
+                <Link to="/admin/products" className = "block py-2 px-4 hover:bg-gray-700">Products</Link>
+                <Link to="/admin/users" className = "block py-2 px-4 hover:bg-gray-700">Users</Link>
+                <Link to="/admin/reviews" className = "block py-2 px-4 hover:bg-gray-700">Reviews</Link>
+
+            </div>
+            <div className = "w-[calc(100%-300px)] h-full border-[10px] border-accent rounded-2xl bg-primary">
+                <Routes>
+                    <Route path = "/" element = {<h1>Orders Dashboard</h1>}/>
+                    <Route path = "/products" element = {<h1>Product Dashboard</h1>}/>
+                    <Route path = "/users" element = {<h1>Users Dashboard</h1>} />
+                    <Route path = "/reviews" element = {<h1>Reviews Dashboard</h1>}/>
+
+                </Routes>
+
+            </div>
+            
+        </div>
+    )
+}
